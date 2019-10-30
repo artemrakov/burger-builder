767852fea6030197abc6bdb427dd1e61dbc4c56d
Artem Rakov <scoff1k@gmail.com>
2019-10-30 18:37:21 -0400

    added burger component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -5,15 +5,15 @@ import classes from './BurgerIngredient.module.css';
 
 class BurgerIngredient extends React.Component {
   burgerIngredients = {
-    bread-bottom: <div className={classes.BreadBottom}></div>,
-    bread-top: (<div className={classes.BreadTop}>
+    'bread-bottom': <div className={classes.BreadBottom}></div>,
+    'bread-top': (<div className={classes.BreadTop}>
                     <div className={classes.Seeds1}></div>
                     <div className={classes.Seeds2}></div>
                 </div>),
-    meat: <div className={classes.Meat}></div>,
-    cheese: <div className={classes.Cheese}></div>,
-    bacon: <div className={classes.Bacon}></div>,
-    salad: <div className={classes.Salad}></div>,
+    'meat': <div className={classes.Meat}></div>,
+    'cheese': <div className={classes.Cheese}></div>,
+    'bacon': <div className={classes.Bacon}></div>,
+    'salad': <div className={classes.Salad}></div>,
   }
 
   render() {
