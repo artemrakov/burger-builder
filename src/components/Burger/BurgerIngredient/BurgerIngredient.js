3d300638f4dddedfeccb46c01340cc0b2c8a08e6
Artem Rakov <scoff1k@gmail.com>
2019-10-30 18:14:55 -0400

    created burgerIngredient component

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './BurgerIngredient.module.css';
+
+class BurgerIngredient extends React.Component {
+  burgerIngredients = {
+    bread-bottom: <div className={classes.BreadBottom}></div>,
+    bread-top: (<div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>),
+    meat: <div className={classes.Meat}></div>,
+    cheese: <div className={classes.Cheese}></div>,
+    bacon: <div className={classes.Bacon}></div>,
+    salad: <div className={classes.Salad}></div>,
+  }
+
+  render() {
+    return this.burgerIngredients[this.props.type];
+  }
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
