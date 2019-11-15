fd65e037931c47d662e93add83dddcf8d5f2d8ef
Artem Rakov <scoff1k@gmail.com>
2019-11-15 12:11:54 -0500

    created orders container and added to routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './container/Orders/Orders';
 
 class App extends React.Component {
   render() {
@@ -11,10 +12,11 @@ class App extends React.Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
         </Layout>
       </div>
     );
   }
 }
 
-export default App;
+export default App
