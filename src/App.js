71c5abb8ae1ce32031a385d68b61ec3576ce5283
Artem Rakov <scoff1k@gmail.com>
2019-11-14 22:34:43 -0500

    added basic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
       </div>
     );
